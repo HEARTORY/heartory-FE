f7fd4957d184bf3c7fb8e95023b58624d5804073
baotam546 <donguyenbaotam546@gmail.com>
2024-05-03 10:02:57 +0700

    [TamDNB] add motion

--- src/router/home/components/FeaturesSession.tsx
+++ src/router/home/components/FeaturesSession.tsx
@@ -1,19 +1,32 @@
 import React from "react";
-
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 import {
   areaInfo
   // , userInfo
   // , quoNum 
 } from "../constants";
+import { slideInFromBottom, slideInFromTop } from "@/utils/motion/motion";
 
 const FeaturesSession: React.FC = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Only trigger once
+    threshold: 0.3, // Trigger animation when 50% of the element is in view
+  });
   return (
     <div>
       <div className="font-bold text-[#EF5DA8] text-4xl mb-10">FEATURES WE PROVIDE</div>
       <div className="flex flex-wrap justify-between">
         {areaInfo.map((area) => (
-          <div className="max-w-[47%] mb-14">
-            <div className="flex flex-wrap gap-5 mb-3">
+          <motion.div 
+          initial="hidden"
+          animate="visible"
+          ref={ref}
+          className="max-w-[47%] mb-14">
+            <motion.div 
+            animate={inView ? "visible" : "hidden"}
+            variants={slideInFromTop(0.3)}
+            className="flex flex-wrap gap-5 mb-3">
               <img
                 className="w-[100px] h-[100px]"
                 src={area.src}
@@ -24,11 +37,14 @@ const FeaturesSession: React.FC = () => {
                   {area.name}
                 </div>
               </div>
-            </div >
-            <span className="text-[#8A8585] text-2xl font-bold">
+            </motion.div >
+            <motion.span
+            animate={inView ? "visible" : "hidden"}
+            variants={slideInFromBottom(0.6)}
+            className="text-[#8A8585] text-2xl font-bold">
               <p>{area.description}</p>
-            </span>
-          </div>
+            </motion.span>
+          </motion.div>
         ))}
       </div>
     </div>
