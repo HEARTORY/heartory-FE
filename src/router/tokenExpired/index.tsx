a9bc9b996ea3e399e4624e68decdb2f529321f86
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2024-05-31 16:52:29 +0700

    [HuyNB] responsive update

--- src/router/tokenExpired/index.tsx
+++ src/router/tokenExpired/index.tsx
@@ -6,11 +6,11 @@ interface Props {
 
 const ErrorTokenPage: React.FC<Props> = () => {
     return (
-        <div className="flex items-center justify-center flex-col ">
-            <img src={Unauthorized} className="w-[30%]" />
-            <h1 className="text-sm sm:text-2xl w-[70%] text-center m-4 text-[#38419D]">
+        <div className="flex h-screen sm:h-auto items-center sm:justify-center flex-col sm:flex-col-reverse">
+            <h1 className="mt-24 sm:mt-4 text-base sm:text-2xl w-[70%] text-center m-4 text-[#38419D] font-medium">
                 Sorry, this request was expired or your password was reset already.
             </h1>
+            <img src={Unauthorized} className="w-[82%] sm:w-[30%]" />
         </div>
     );
 };
